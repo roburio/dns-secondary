3d98b42efa0227f367b89aae302218d48489a8d6
Hannes Mehnert <hannes@mehnert.org>
2020-08-01 18:18:08 +0200

    monitoring and syslog for deployment

--- config.ml
+++ config.ml
@@ -6,19 +6,38 @@ let keys =
   let doc = Key.Arg.info ~doc:"nsupdate keys (name:type:value,...)" ["keys"] in
   Key.(create "keys" Arg.(opt (list string) [] doc))
 
+let monitor =
+  let doc = Key.Arg.info ~doc:"monitor host IP" ["monitor"] in
+  Key.(create "monitor" Arg.(opt (some ip_address) None doc))
+
+let syslog =
+  let doc = Key.Arg.info ~doc:"syslog host IP" ["syslog"] in
+  Key.(create "syslog" Arg.(opt (some ip_address) None doc))
+
+let name =
+  let doc = Key.Arg.info ~doc:"Name of the unikernel" ["name"] in
+  Key.(create "name" Arg.(opt string "sn.nqsb.io" doc))
+
 let dns_handler =
   let packages =
     [
       package "logs" ;
       package ~min:"5.0.0" ~sublibs:["mirage"] "dns-server";
       package "dns-tsig";
+      package ~min:"0.0.2" "monitoring-experiments";
+      package ~sublibs:["mirage"] ~min:"0.3.0" "logs-syslog";
     ]
-  and keys = Key.([ abstract keys ])
+  and keys = [
+    Key.abstract keys ;
+    Key.abstract name ; Key.abstract monitor ; Key.abstract syslog
+  ]
   in
   foreign
     ~keys
     ~packages
-    "Unikernel.Main" (random @-> pclock @-> mclock @-> time @-> stackv4v6 @-> job)
+    "Unikernel.Main" (console @-> random @-> pclock @-> mclock @-> time @-> stackv4v6 @-> stackv4v6 @-> job)
+
+let management_stack = generic_stackv4v6 ~group:"management" (netif ~group:"management" "management")
 
 let () =
-  register "secondary" [dns_handler $ default_random $ default_posix_clock $ default_monotonic_clock $ default_time $ generic_stackv4v6 default_network ]
+  register "secondary" [dns_handler $ default_console $ default_random $ default_posix_clock $ default_monotonic_clock $ default_time $ generic_stackv4v6 default_network $ management_stack ]
